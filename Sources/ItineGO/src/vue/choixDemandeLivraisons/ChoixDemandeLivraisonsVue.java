12b345f3c0c133dceb7cde25aea44a150592737a
Quentin Vecchio <vecchioquentin@hotmail.fr>
2016-10-15 15:20:48 +0200

    Modification de la vue choix demande de livraisons

--- Sources/ItineGO/src/vue/choixDemandeLivraisons/ChoixDemandeLivraisonsVue.java
+++ Sources/ItineGO/src/vue/choixDemandeLivraisons/ChoixDemandeLivraisonsVue.java
@@ -1,19 +1,36 @@
 package vue.choixDemandeLivraisons;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import controleur.Controleur;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
+import modeles.Plan;
+import vue.PlanVilleVue.PlanVilleVue;
 
 public class ChoixDemandeLivraisonsVue implements Initializable{
 	private Controleur controleur;
+	private Plan plan;
+	private File fichierChoisie = null;
 	
 	@FXML
 	private TextField textFieldLienFichier;
+	
+	@FXML
+	private Label labelError;
 	
 	@FXML
 	private Button boutonParcourirFichier;
@@ -21,10 +38,127 @@ public class ChoixDemandeLivraisonsVue implements Initializable{
 	@FXML
 	private Button boutonValider;
 	
+	@FXML
+	private StackPane contentPane;
+	
+	@FXML 
+	private PlanVilleVue planVilleVue;
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+		contentPane.setOnDragOver(new EventHandler<DragEvent>() {
+            @Override
+            public void handle(final DragEvent event) {
+                mouseDragOver(event);
+            }
+        });
+ 
+        contentPane.setOnDragDropped(new EventHandler<DragEvent>() {
+            @Override
+            public void handle(final DragEvent event) {
+                mouseDragDropped(event);
+            }
+        });
+ 
+        contentPane.setOnDragExited(new EventHandler<DragEvent>() {
+            @Override
+            public void handle(final DragEvent event) {
+                contentPane.setStyle("-fx-border-color: #C6C6C6;");
+            }
+        });
+        
+        labelError.setVisible(false);
 	}
+	
+	public void afficherErreur(String erreur) {
+		labelError.setDisable(false);
+		labelError.setText(erreur);
+	}
+	
+	@FXML
+	public void choixFichierAction(MouseEvent event) {
+		FileChooser dialogue = new FileChooser();
+		FileChooser.ExtensionFilter extensionsFilter = new FileChooser.ExtensionFilter("Fichier XML","*.xml");
+		dialogue.setSelectedExtensionFilter(extensionsFilter);
+		File tmp = dialogue.showOpenDialog(controleur.getStage());
+		if(tmp.getName().toLowerCase().endsWith(".xml")) {
+        	fichierChoisie = tmp;
+        } else {
+        	fichierChoisie = null;
+        }
+		if(fichierChoisie != null) {
+			labelError.setVisible(false);
+			textFieldLienFichier.setText(fichierChoisie.getPath());
+		} else {
+			labelError.setVisible(true);
+			labelError.setText("Erreur : Le fichier choisi est invalide");
+		}
+	}
+	
+	@FXML
+	public void validerAction(MouseEvent event) {
+		if(controleur == null) {
+			labelError.setVisible(true);
+			labelError.setText("Erreur : Controler is null, fatal error");
+		} else if(fichierChoisie == null) {
+			labelError.setVisible(true);
+			labelError.setText("Erreur : Le fichier choisi est invalide");
+		}
+	}
+	
+	private void mouseDragDropped(final DragEvent e) {
+        final Dragboard db = e.getDragboard();
+        boolean success = false;
+        if (db.hasFiles()) {
+            success = true;
+            // Only get the first file from the list
+            
+            if(db.getFiles().get(0).getName().toLowerCase().endsWith(".xml")) {
+            	fichierChoisie = db.getFiles().get(0);
+            } else {
+            	fichierChoisie = null;
+            }
+            Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                	if(fichierChoisie != null) {
+                		labelError.setVisible(false);
+                		textFieldLienFichier.setText(fichierChoisie.getAbsolutePath());
+                	} else {
+                		textFieldLienFichier.setText("");
+                		labelError.setVisible(true);
+            			labelError.setText("Erreur : Le fichier choisi est invalide");
+                	}
+                }
+            });
+        }
+        e.setDropCompleted(success);
+        e.consume();
+    }
+ 
+    private  void mouseDragOver(final DragEvent e) {
+        final Dragboard db = e.getDragboard();
+ 
+        final boolean isAccepted = db.getFiles().get(0).getName().toLowerCase().endsWith(".xml");
+ 
+        if (db.hasFiles()) {
+            if (isAccepted) {
+                contentPane.setStyle("-fx-border-color: green;"
+              + "-fx-border-width: 5;"
+              + "-fx-background-color: #C6C6C6;"
+              + "-fx-border-style: solid;");
+                e.acceptTransferModes(TransferMode.COPY);
+            } else {
+                contentPane.setStyle("-fx-border-color: red;"
+              + "-fx-border-width: 5;"
+              + "-fx-background-color: #C6C6C6;"
+              + "-fx-border-style: solid;");
+                e.acceptTransferModes(TransferMode.COPY);
+            }
+        } else {
+        	e.consume();
+        }
+    }
 	
 	public Controleur getControleur() {
 		return controleur;
@@ -32,5 +166,17 @@ public class ChoixDemandeLivraisonsVue implements Initializable{
 	
 	public void setControleur(Controleur controleur) {
 		this.controleur = controleur;
+	}
+
+	public Plan getPlan() {
+		return plan;
+	}
+
+	public void setPlan(Plan plan) {
+		this.plan = plan;
+	}
+	
+	public File getFichierChoisi() {
+		return fichierChoisie;
 	}
 }
