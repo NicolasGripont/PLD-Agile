d4d20f18ba7d74690f12b578014feb42a0256e00
Pierre JAGLIN <pjjaglin@gmail.com>
2016-10-18 11:07:02 +0200

    Ajout des Test XML pour le Parser

---
+++ Sources/ItineGO/tests/modelesTests/ParserPlanTest.java
@@ -0,0 +1,14 @@
+package modelesTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ParserPlanTest {
+
+	@Test
+	public void testParserPlanVille() {
+		fail("Not yet implemented");
+	}
+
+}
