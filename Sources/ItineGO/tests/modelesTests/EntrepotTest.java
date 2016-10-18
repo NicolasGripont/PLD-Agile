41ad094f5bd21741ec79b15e9237de5a6f64dacb
Quentin Vecchio <vecchioquentin@hotmail.fr>
2016-10-18 10:42:53 +0200

    Ajout du test de la classe Entrepot

--- Sources/ItineGO/tests/modelesTests/EntrepotTest.java
+++ Sources/ItineGO/tests/modelesTests/EntrepotTest.java
@@ -1,65 +1,81 @@
 package modelesTests;
 
 import static org.junit.Assert.*;
-import org.hamcrest.*;
 import org.junit.Test;
 import modeles.Entrepot;
+import modeles.Horaire;
 import modeles.Noeud;
 
 public class EntrepotTest {
-
-	@Test
-	public void testHashCode() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	public void testEntrepotNoeudIntIntInt() {
 		Noeud noeud = new Noeud(12,12,12);
 		Entrepot entrepot = new Entrepot(noeud, 10, 20, 29);
 		String res1 = "Entrepot [noeud=Noeud [id=12, x=12, y=12], horaireDepart=10:20:29]";
-		String res2 = entrepot.toString();
-		assertEquals(res1,res2);
+		assertEquals(res1,entrepot.toString());
+	}
+	
+	@Test
+	public void testEntrepotHoraireImpossible() {
+		Noeud noeud = new Noeud(12,12,12);
+		Entrepot entrepot = new Entrepot(noeud, 25, 70, 100);
+		String res1 = "Entrepot [noeud=Noeud [id=12, x=12, y=12], horaireDepart=0:0:0]";
+		assertEquals(res1,entrepot.toString());
 	}
 
 	@Test
 	public void testEntrepotNoeudHoraire() {
-		fail("Not yet implemented");
+		Horaire horaire = new Horaire(10, 20, 29);
+		Noeud noeud = new Noeud(12,12,12);
+		Entrepot entrepot = new Entrepot(noeud, horaire);
+		String res1 = "Entrepot [noeud=Noeud [id=12, x=12, y=12], horaireDepart=10:20:29]";
+		assertEquals(res1, entrepot.toString());
 	}
 
 	@Test
 	public void testEntrepotNoeudString() {
-		fail("Not yet implemented");
+		Noeud noeud = new Noeud(12,12,12);
+		Entrepot entrepot = new Entrepot(noeud, "10:20:29");
+		String res1 = "Entrepot [noeud=Noeud [id=12, x=12, y=12], horaireDepart=10:20:29]";
+		assertEquals(res1, entrepot.toString());
+	}
+	
+	@Test
+	public void testEntrepotNoeudStringMauvaisFormat() {
+		Noeud noeud = new Noeud(12,12,12);
+		Entrepot entrepot = new Entrepot(noeud, "helloworld!:20:20");
+		String res1 = "Entrepot [noeud=Noeud [id=12, x=12, y=12], horaireDepart=0:0:0]";
+		assertEquals(res1, entrepot.toString());
 	}
 
 	@Test
-	public void testGetNoeud() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetNoeud() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetHoraireDepart() {
-		fail("Not yet implemented");
+	public void testSetNoeudNull() {
+		Noeud noeud = new Noeud(12,12,12);
+		Entrepot entrepot = new Entrepot(noeud, "10:20:29");
+		entrepot.setNoeud(null);
+		String res1 = "Entrepot [noeud=Noeud [id=-1, x=0, y=0], horaireDepart=10:20:29]";
+		assertEquals(res1, entrepot.toString());
 	}
 
 	@Test
 	public void testSetHoraireDepart() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testToString() {
-		fail("Not yet implemented");
+		Noeud noeud = new Noeud(12,12,12);
+		Entrepot entrepot = new Entrepot(noeud, "10:20:29");
+		entrepot.setHoraireDepart(null);
+		String res1 = "Entrepot [noeud=Noeud [id=12, x=12, y=12], horaireDepart=0:0:0]";
+		assertEquals(res1, entrepot.toString());
 	}
 
 	@Test
 	public void testEqualsObject() {
-		fail("Not yet implemented");
+		Noeud noeud = new Noeud(12,12,12);
+		Entrepot entrepot1 = new Entrepot(noeud, "10:20:29");
+		Entrepot entrepot2 = new Entrepot(noeud, "10:20:29");
+		Noeud noeud2 = new Noeud(13,16,12);
+		Entrepot entrepot3 = new Entrepot(noeud2, "10:20:29");
+		assertEquals(entrepot1, entrepot2);
+		assertNotEquals(entrepot1, entrepot3);
 	}
 
 }
