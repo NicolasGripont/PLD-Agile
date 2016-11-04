e61510916afde736890d907dcec3054a2314d42a
Nicolas Chazeau <n.chazeau@laposte.net>
2016-11-04 08:16:22 +0100

    Correction BadXmlPlan

--- Sources/ItineGO/src/exceptions/BadXmlLivraison.java
+++ Sources/ItineGO/src/exceptions/BadXmlLivraison.java
@@ -7,7 +7,7 @@ public class BadXmlLivraison extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public BadXmlLivraison(String message) {
-		super("Livraisons non valides");
+		super(message);
 	}
 	
 	public BadXmlLivraison() {
