826c019c5034367911cb4477f54a6ed4214447ca
KaiserCroc <piebay13@laposte.net>
2016-11-07 15:49:40 +0100

    Modif suites aux modifs sur l'UML

--- Sources/ItineGO/src/controleur/EtatAjouterTourneeOrdre.java
+++ Sources/ItineGO/src/controleur/EtatAjouterTourneeOrdre.java
@@ -1,5 +1,18 @@
 package controleur;
 
+import modeles.Gestionnaire;
+import modeles.Noeud;
+
 public class EtatAjouterTourneeOrdre extends EtatDefaut {
 
+	/**
+	 * On répond à l'action d'un clique sur une Livraison.
+	 * 
+	 * @param controleur
+	 * @param gestionnaire
+	 * @param noeud
+	 * @param numLigne
+	 */
+	public void clicPlanLivraison(Controleur controleur, Gestionnaire gestionnaire, Noeud noeud, int numLigne) {
+	}
 }
