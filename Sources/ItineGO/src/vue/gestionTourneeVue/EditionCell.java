131b95a98803ebd116c72587774db9c0bc20ba3a
Quentin Vecchio <vecchioquentin@hotmail.fr>
2016-11-08 19:28:55 +0100

    Ajout du putain de focus sur la cellule

--- Sources/ItineGO/src/vue/gestionTourneeVue/EditionCell.java
+++ Sources/ItineGO/src/vue/gestionTourneeVue/EditionCell.java
@@ -1,56 +1,93 @@
 package vue.gestionTourneeVue;
 
-import javafx.beans.value.ChangeListener;import javafx.beans.value.ObservableValue;
-import javafx.scene.control.ContentDisplay;
-import javafx.scene.control.Label;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.control.TableCell;
-import javafx.scene.control.TablePosition;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import modeles.Livraison;
 
-public class EditionCell  extends TableCell<Livraison, String>{
-	private final TextField textField = new TextField();
-	private final Label label = new Label();
-	private TableView<Livraison> table;
-	
-	EditionCell(TableView<Livraison> table) { 
-		this.table = table;
-        textProperty().bind(itemProperty());
-        setGraphic(textField);
-        focusedProperty().addListener(new ChangeListener<Boolean>() {
-            @Override
-            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-                if (newValue.booleanValue()) {
-                	textField.setText("Toto");
-                	KeyEvent press = new KeyEvent(textField, textField, KeyEvent.KEY_PRESSED, "", "", KeyCode.BACK_SPACE, false, false, false, false);
-                	textField.fireEvent(press);
-                	//textField.requestFocus();
-                }  
+class EditionCell extends TableCell<Livraison, String> {
+
+    private TextField textField;
+
+    public EditionCell() {
+    }
+
+    @Override
+    public void startEdit() {
+        if (!isEmpty()) {
+            super.startEdit();
+            createTextField();
+            setText(null);
+            setGraphic(textField);
+            textField.requestFocus();
+            textField.selectAll();
+        } 
+    }
+
+    @Override
+    public void cancelEdit() {
+        super.cancelEdit();
+        setText((String) getItem());
+        setGraphic(null);
+    }
+
+    @Override
+    public void updateItem(String item, boolean empty) {
+        super.updateItem(item, empty);
+
+        if (empty) {
+            setText(null);
+            setGraphic(null);
+        } else {
+            if (isEditing()) {
+                if (textField != null) {
+                    textField.setText(getString());
+                }
+                setText(null);
+                setGraphic(textField);
+            } else {
+                setText(getString());
+                setGraphic(null);
+            }
+        }
+    }
+
+    private void createTextField() {
+
+        textField = new TextField(getString());
+        textField.setMinWidth(this.getWidth() - this.getGraphicTextGap() * 2);
+
+        // commit on focus lost
+        textField.focusedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+
+            if( oldValue = true && newValue == false) {
+
+                System.out.println( "Focus lost, current value: " + textField.getText());
+
+                commitEdit();
+
             }
         });
+
+        // cancel edit on ESC
+        textField.addEventFilter(KeyEvent.KEY_RELEASED, e -> {
+
+            if( e.getCode() == KeyCode.ESCAPE) {
+                cancelEdit();
+            }
+
+        });
+
     }
-	
-	@Override
-	protected void updateItem(String item, boolean empty) {
-		super.updateItem(item, empty);
-		if (!empty) {
-			setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
-			if(getIndex() == (table.getItems().size()-1)) {
-				setGraphic(textField);
-				textField.setText(item);
-			}
-			else {
-				setGraphic(label);
-				label.setText(item);
-			}
-				
-		} else {
-			setGraphic(null);
-		}
-	}
-	
-	
+
+    private String getString() {
+        return getItem() == null ? "" : getItem().toString();
+    }
+
+    private boolean commitEdit() {
+        super.commitEdit(textField.getText());
+        return true; // TODO: add verifier and check if commit was possible
+    }
 }
