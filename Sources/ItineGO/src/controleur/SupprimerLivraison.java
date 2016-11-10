d160125de00edf8014d8fc43596c71407164bc07
KaiserCroc <piebay13@laposte.net>
2016-11-10 23:15:31 +0100

    Ajout de commentaires

--- Sources/ItineGO/src/controleur/SupprimerLivraison.java
+++ Sources/ItineGO/src/controleur/SupprimerLivraison.java
@@ -5,6 +5,9 @@ import modeles.Gestionnaire;
 import modeles.Livraison;
 import modeles.Noeud;
 
+/**
+ * Commande de suppression d'une livraison.
+ */
 public class SupprimerLivraison extends Commande {
 
 	private Livraison livraison;
@@ -12,11 +15,23 @@ public class SupprimerLivraison extends Commande {
 	private Noeud noeudSuivant;
 	private final int numLigne;
 
+	/**
+	 * Constructeur de la commande.
+	 * 
+	 * @param gestionnaire
+	 *            : Gestionnaire de l'application.
+	 * @param numLigne
+	 *            : Position de la livraison à supprimer.
+	 */
 	public SupprimerLivraison(Gestionnaire gestionnaire, int numLigne) {
 		this.numLigne = numLigne;
 		this.gestionnaire = gestionnaire;
 	}
 
+	/**
+	 * Appel la méthode de suppression du modèle.
+	 * On stocke la livraison ainsi que le noeud suivant cette livraison en cas de undo.
+	 */
 	@Override
 	public void doCode() throws NonRespectPlagesHoraires {
 		this.livraison = this.gestionnaire.getLivraisonTournee(this.numLigne);
@@ -24,6 +39,10 @@ public class SupprimerLivraison extends Commande {
 		this.gestionnaire.supprimerLivraisonTournee(this.numLigne);
 	}
 
+	/**
+	 * Appel la méthode d'ajout du modèle.
+	 * On récupère la livraison stockée avant l'appel.
+	 */
 	@Override
 	public void undoCode() throws NonRespectPlagesHoraires {
 		this.gestionnaire.setLivraisonEnCourCreation(this.livraison);
