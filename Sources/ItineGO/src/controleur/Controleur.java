f1ad5e18e14bc6b21dbcfa69c72776d352ee7e10
LudovicCros <cros_ludovic@hotmail.fr>
2016-11-10 01:00:11 +0100

    Modification taille ihm

--- Sources/ItineGO/src/controleur/Controleur.java
+++ Sources/ItineGO/src/controleur/Controleur.java
@@ -75,7 +75,7 @@ public class Controleur extends Application {
 		setEtatCourant(etatApplicationDemarree);
 		stage = primaryStage;
 		stage.setMinWidth(900); //	Largeur minimum fixée
-		stage.setMinHeight(600); // Hauteur minimum fixée
+		stage.setMinHeight(620); // Hauteur minimum fixée
 		try {
 			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/vue/choixPlanVilleVue/ChoixPlanVille.fxml"));
 			Parent root;
